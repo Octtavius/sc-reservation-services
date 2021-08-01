af14e2520e77e8543535803aa022c9b68afc65bd
Octavian Morcov <octavian.morcov@workhuman.com>
2021-08-01 10:40:08 +0100

    basic web app

---
+++ src/main/java/com/wh/controllers/ReservationController.java
@@ -0,0 +1,29 @@
+package com.wh.controllers;
+
+import com.wh.data.Reservation;
+import com.wh.repositories.ReservationRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    private final ReservationRepository repository;
+
+    public ReservationController(ReservationRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Iterable<Reservation> getAllReservations() {
+        return this.repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Reservation getReservation(@PathVariable("id") long id) {
+        return this.repository.findById(id).get();
+    }
+}
---
+++ src/main/java/com/wh/repositories/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.wh.repositories;
+
+import com.wh.data.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+}
